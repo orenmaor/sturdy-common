eeec21b6fab584dd2cd68de820e667d2869af8bb
root <root@chef.sturdynetworks.com>
2014-11-20 08:39:15 -0800

    added hosts

---
+++ host/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: host
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template '/tmp/test' do
+  source "test.erb"
+end
